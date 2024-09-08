174cd50d4b3a4dae94d7c3615e01e8de657dc630
Ansuman Dhar <ansuman.official100@gmail.com>
2024-09-08 19:43:34 +0530

    configured new routes

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:mobilepos/pages/onboarding.dart';
 import 'package:mobilepos/pages/home.dart';
-
+import 'package:mobilepos/pages/onboarding.dart';
+import 'package:mobilepos/pages/inventory/inventory_add.dart';
+import 'package:mobilepos/pages/sales/sale_new.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
@@ -12,6 +13,10 @@ class RouteGenerator {
           return const HomePage();
         case '/onboarding':
           return const OnBoarding();
+        case '/addtoinventory':
+          return const AddInventory();
+        case '/newsale':
+          return const NewSale();
         default:
           return const HomePage();
       }
